f327572f51cde4e1c1fff0cc875563710ba20141
chulhee23 <chulhee2351@gmail.com>
2021-03-16 16:19:09 +0900

    모래시계

---
+++ 02control_structure/hw_2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void draw(int i, int row);
+
+int main()
+{
+  int row;
+  int k;
+
+  cin >> row;
+
+  for (int i = 0; i < row; i++)
+  {
+    if (i * 2 < row)
+      draw(i, row);
+    else
+    {
+      k = row - i - 1;
+      draw(k, row);
+    }
+  }
+
+  return 0;
+}
+
+void draw(int i, int row)
+{
+  {
+    for (int j = 0; j < i; j++)
+      cout << "-";
+
+    for (int j = 0; j < row - i * 2; j++)
+      if (j % 2 == 0)
+        cout << "*";
+      else
+        cout << "+";
+
+    for (int j = 0; j < i; j++)
+      cout << "-";
+    cout << endl;
+  }
+}
