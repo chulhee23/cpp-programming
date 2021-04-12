0a6a3581d5a4c43f10d1b6536fd3502e8c7dbd5a
chulhee23 <chulhee2351@gmail.com>
2021-04-12 21:41:38 +0900

    기하 넓이

--- week6/hw/2_area.cpp
+++ week6/hw/2_area.cpp
@@ -1,47 +1,37 @@
 #include <iostream>
-#include <string>
-#include <cmath>
-#include <typeinfo>
 using namespace std;
 
-int main()
+int main(int argc, char const *argv[])
 {
-
   int n;
   cin >> n;
   int *x = new int[n];
   int *y = new int[n];
   for (int i = 0; i < n; i++)
     cin >> x[i] >> y[i];
-  
-  int signedArea = 0;
-  int area;
+
+  int area = 0;
 
   for (int i = 0; i < n; i++)
   {
     int tmp;
-    if(i + 1 == n){
+    if (i + 1 == n) {
       tmp = (x[i] + x[0]) * (y[0] - y[i]);
+    }
 
-    } 
     else {
       tmp = (x[i] + x[i + 1]) * (y[i + 1] - y[i]);
-
     }
-    signedArea += tmp;
+    area += tmp;
+
   }
+  int clockwise = 1;
+  if (area < 0)
+  {
+    clockwise = -1;
+  }
+  int ans = abs(area);
+  cout << ans << " " << clockwise;
 
-  signedArea /= 2;
-  area = 2 * abs(signedArea);
 
-  int check;
-  if (signedArea < 0)
-    check = -1;
-  else
-    check = 1;
-
-  cout << area << " " << check << endl;
-
-  return 0;
 }
-
